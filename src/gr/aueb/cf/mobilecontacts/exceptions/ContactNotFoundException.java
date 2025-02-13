b5130dee53d9151cbc43ceb80b409fd7ffbf34f4
Marinos.Sake <protutor13@hotmail.com>
2025-02-13 03:51:17 +0200

    My new project

---
+++ src/gr/aueb/cf/mobilecontacts/exceptions/ContactNotFoundException.java
@@ -0,0 +1,10 @@
+package gr.aueb.cf.mobilecontacts.exceptions;
+
+public class ContactNotFoundException extends Exception {
+
+
+    public ContactNotFoundException(String message) {
+        super(message);
+    }
+
+}
